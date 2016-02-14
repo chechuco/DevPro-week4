4ab60874a644d546cbce75be12e97774b9c1a747
arcadianlyric <yingcai1123@gmail.com>
2016-02-13 20:33:08 -0500

    assignment

--- ui.R
+++ ui.R
@@ -1,20 +1,16 @@
 # develope R product, week4assignment 
 #setwd('/Users/YC/github/DevPro-week4')
-# install an older version of shiny
-#devtools::install_github("rstudio/shiny-incubator", ref = "0f343efe9b6c255e6687aaa52b5443cf975f96e7")
+
 # progressInit issue
 #devtools::install_github('rstudio/rsconnect')
 library(shiny)
 library(shinyIncubator)
 library(googleVis)
+# install an older version of shiny to solve processInit issue
+devtools::install_github("rstudio/shiny-incubator", ref = "0f343efe9b6c255e6687aaa52b5443cf975f96e7")
 #library(rsconnect)
 #rsconnect::setAccountInfo(name="<ACCOUNT>", token="<TOKEN>", secret="<SECRET>")
 #deployApp()
-progressInit= function () {
-    addResourcePath("progress", system.file("progress", package = "shinyIncubator"))
-    tagList(singleton(tags$head(tags$script(src = "progress/progress.js"), 
-                                tags$link(rel = "stylesheet", type = "text/css", href = "progress/progress.css"))))
-}
 
 shinyUI(fluidPage(
     headerPanel("San Francisco Crime Data"),
@@ -26,5 +22,5 @@ p("The SF crime data is from", a(href="https://data.sfgov.org/Public-Safety/SFPD
         htmlOutput("gvis")
         ),
     sidebarPanel(
-        checkboxGroupInput("crime_categories", label="Select one or more categories of crime to show on map.", c("ARSON" ="ARSON", "ASSAULT" ="ASSAULT", "BAD CHECKS" ="BAD CHECKS", "BRIBERY" ="BRIBERY", "BURGLARY" ="BURGLARY", "DISORDERLY CONDUCT" ="DISORDERLY CONDUCT", "DRIVING UNDER THE INFLUENCE" ="DRIVING UNDER THE INFLUENCE", "DRUG/NARCOTIC" ="DRUG/NARCOTIC", "DRUNKENNESS" ="DRUNKENNESS" ,"EMBEZZLEMENT" ="EMBEZZLEMENT", "EXTORTION" ="EXTORTION", "FAMILY OFFENSES" ="FAMILY OFFENSES", "FORGERY/COUNTERFEITING" ="FORGERY/COUNTERFEITING", "FRAUD" ="FRAUD", "GAMBLING" ="GAMBLING" ,"KIDNAPPING" ="KIDNAPPING", "LARCENY/THEFT" ="LARCENY/THEFT", "LIQUOR LAWS" ="LIQUOR LAWS", "LOITERING" ="LOITERING","MISSING PERSON" ="MISSING PERSON", "NON-CRIMINAL"="NON-CRIMINAL" ,"OTHER OFFENSES" ="OTHER OFFENSES", "PORNOGRAPHY/OBSCENE MAT" ="PORNOGRAPHY/OBSCENE MAT", "PROSTITUTION" ="PROSTITUTION", "ROBBERY" ="ROBBERY" , "RUNAWAY" ="RUNAWAY", "SECONDARY CODES" ="SECONDARY CODES", "SEX OFFENSES, FORCIBLE" ="SEX OFFENSES, FORCIBLE", "SEX OFFENSES, NON FORCIBLE" ="SEX OFFENSES, NON FORCIBLE", "STOLEN PROPERTY" ="STOLEN PROPERTY", "SUICIDE" ="SUICIDE" , "SUSPICIOUS OCC" ="SUSPICIOUS OCC", "TREA" ="TREA", "TRESPASS" ="TRESPASS", "VANDALISM" ="VANDALISM" ,"VEHICLE THEFT" ="VEHICLE THEFT", "WARRANTS" ="WARRANTS", "WEAPON LAWS" ="WEAPON LAWS")))
+        checkboxGroupInput("crime_categories", label="Select one or more categories of crime to show on map.", c("ARSON" ="ARSON", "ASSAULT" ="ASSAULT", "BAD CHECKS" ="BAD CHECKS", "BRIBERY" ="BRIBERY", "BURGLARY" ="BURGLARY", "DISORDERLY CONDUCT" ="DISORDERLY CONDUCT", "DRIVING UNDER THE INFLUENCE" ="DRIVING UNDER THE INFLUENCE", "DRUG/NARCOTIC" ="DRUG/NARCOTIC", "DRUNKENNESS" ="DRUNKENNESS" ,"EMBEZZLEMENT" ="EMBEZZLEMENT", "EXTORTION" ="EXTORTION", "FAMILY OFFENSES" ="FAMILY OFFENSES", "FORGERY/COUNTERFEITING" ="FORGERY/COUNTERFEITING", "FRAUD" ="FRAUD", "GAMBLING" ="GAMBLING" ,"KIDNAPPING" ="KIDNAPPING", "LARCENY/THEFT" ="LARCENY/THEFT", "LIQUOR LAWS" ="LIQUOR LAWS", "LOITERING" ="LOITERING")))
 ))
