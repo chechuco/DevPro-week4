f3ef168d2dbced9c17b28ffbb3cfd674ff92068a
arcadianlyric <yingcai1123@gmail.com>
2016-02-13 22:18:38 -0500

    assingment

--- server.R
+++ server.R
@@ -6,26 +6,26 @@ setwd('/Users/YC/github/DevPro-week4')
 library(shiny)
 library(shinyIncubator)
 library(googleVis)
-
-# SF crime data from https://data.sfgov.org/Public-Safety/SFPD-Incidents-from-1-January-2003/tmnf-yvry
+library(XLConnect)
+# ak volcano data from https://www.avo.alaska.edu/volcanoes/latlong.php
 # filtered out 2015 incidents
 # preprocess data
-sf= read.csv("sf-2015-dec.csv", na.strings="")
+
+wb= loadWorkbook('/Users/YC/github/DevPro-week4/AKvolclatlong.xls')
+ak = readWorksheet(wb, sheet = getSheets(wb))
 
 # Format lattitude and longitude
-sf$loc= paste(sf$Y, ':', sf$X, sep='')
-sf$detail= paste(sf$Category, "on", as.character(sf$Date), "at", as.character(sf$Time))
+ak$loc= paste(ak$Latitude, ':', ak$Longitude, sep='')
+ak$detail= paste(ak$Volcano,  "at", as.character(ak$Latitude),":",as.character(ak$Longitude))
 
 # set app
 shinyServer(function(input, output, session) {
     output$gvis= renderGvis({
          withProgress(session, {
-            setProgress(message = "Calculating, please wait",
-                        detail = "This may take a while...")
             
-            sf.crime= sf[(sf$Category %in% input$crime_categories), ]
+            ak.vol= ak[(ak$Volcano %in% input$volcano), ]
             
-            gvisMap(sf.crime, "loc", "detail",
+            gvisMap(ak.vol, "loc", "detail",
                     options=list(showTip=TRUE, 
                                  showLine=TRUE, 
                                  enableScrollWheel=TRUE,
