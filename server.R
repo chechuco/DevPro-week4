07572541ec4569f8fd503f8d82bb03e0c7a0f573
arcadianlyric <yingcai1123@gmail.com>
2016-02-14 21:20:03 -0500

    assignment

--- server.R
+++ server.R
@@ -1,19 +1,20 @@
 # develope R product, week4assignment 
-setwd('/Users/YC/github/DevPro-week4')
+#setwd('/Users/YC/github/DevPro-week4')
 #install.packages("devtools")
 #devtools::install_github("shiny-incubator", "rstudio")
 
 library(shiny)
 library(shinyIncubator)
 library(googleVis)
-library(XLConnect)
+#library(XLConnect)
 # ak volcano data from https://www.avo.alaska.edu/volcanoes/latlong.php
 # filtered out 2015 incidents
 # preprocess data
+#wb= loadWorkbook('/Users/YC/Desktop/DevPro-week4/AKvolclatlong.xls')
+#ak = readWorksheet(wb, sheet = getSheets(wb))
+#save.image('tmp.rda')
 
-wb= loadWorkbook('/Users/YC/github/DevPro-week4/AKvolclatlong.xls')
-ak = readWorksheet(wb, sheet = getSheets(wb))
-
+load('tmp.rda')
 # Format lattitude and longitude
 ak$loc= paste(ak$Latitude, ':', ak$Longitude, sep='')
 ak$detail= paste(ak$Volcano,  "at", as.character(ak$Latitude),":",as.character(ak$Longitude))
